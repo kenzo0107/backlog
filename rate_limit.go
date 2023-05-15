c57eca66d1fb9fca149d61c5aabeedf28b4de275
Kenzo Tanaka <kenzo.tanaka0107@gmail.com>
2023-05-15 16:06:58 +0900

    Support rate limit API
    Support backlog API the below:
    
    - GET /api/v2/rateLimit
      ref: https://developer.nulab.com/ja/docs/backlog/rate-limit/

---
+++ rate_limit.go
@@ -0,0 +1,46 @@
+package backlog
+
+import (
+	"context"
+)
+
+// LimitStatus : limit status
+type LimitStatus struct {
+	Limit     *int `json:"limit,omitempty"`
+	Remaining *int `json:"remaining,omitempty"`
+	Reset     *int `json:"reset,omitempty"`
+}
+
+// RateLimit : rate limit
+type RateLimit struct {
+	Read   *LimitStatus `json:"read,omitempty"`
+	Update *LimitStatus `json:"update,omitempty"`
+	Search *LimitStatus `json:"search,omitempty"`
+	Icon   *LimitStatus `json:"icon,omitempty"`
+}
+
+// ResponseRateLimit : rate limit API response
+type ResponseRateLimit struct {
+	RateLimit *RateLimit `json:"rateLimit,omitempty"`
+}
+
+// GetRateLimit returns the rate limit
+func (c *Client) GetRateLimit() (*RateLimit, error) {
+	return c.GetRateLimitContext(context.Background())
+}
+
+// GetRateLimitContext returns the rate limit
+func (c *Client) GetRateLimitContext(ctx context.Context) (*RateLimit, error) {
+	u := "/api/v2/rateLimit"
+
+	req, err := c.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	responseRateLimit := new(ResponseRateLimit)
+	if err := c.Do(ctx, req, &responseRateLimit); err != nil {
+		return nil, err
+	}
+	return responseRateLimit.RateLimit, nil
+}
